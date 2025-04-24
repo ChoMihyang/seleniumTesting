ca94d865512002068ff73b4c15ccc0267646b6a7
ChoMihyang <chomh316@gmail.com>
2025-04-24 18:52:03 +0900

    feat basic browser launch

---
+++ demoBrowser.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+service = Service(executable_path="/usr/local/bin/chromedriver")
+driver = webdriver.Chrome(service=service)
+
+
+driver.get("https://google.com")
