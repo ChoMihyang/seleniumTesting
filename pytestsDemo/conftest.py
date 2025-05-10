1ed0460cc4069298497385d0be6a3830c26b3f86
ChoMihyang <chomh316@gmail.com>
2025-05-10 23:39:43 +0900

    Test fixtures Data

--- pytestsDemo/conftest.py
+++ pytestsDemo/conftest.py
@@ -6,3 +6,8 @@ def setup():
    print("I will be executing first")
    yield
    print("I will execute last")
+
+@pytest.fixture
+def dataLoad():
+   print("user profile data is being created")
+   return ["one", "two", "three.com"]
