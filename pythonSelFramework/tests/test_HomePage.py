e99060a17f7c50b63e99d770c6d62518b82c295f
ChoMihyang <chomh316@gmail.com>
2025-06-01 17:28:51 +0900

    Create Data Set

--- pythonSelFramework/tests/test_HomePage.py
+++ pythonSelFramework/tests/test_HomePage.py
@@ -1,17 +1,23 @@
 from utilities.baseClass import BaseClass
 from pageObject.homePage import Homepage
-from selenium.webdriver.support.ui import Select
+import pytest
 
 
 
 class TestHomePage(BaseClass):
-    def test_formSubmission(self):
+    def test_formSubmission(self, getData):
         homepage = Homepage(self.driver)
-        homepage.getName().send_keys("My Name")
+        homepage.getName().send_keys(getData[0])
         homepage.getEmail().send_keys("myname@email.com")
         homepage.getCheckbox().click()
         self.selectOptionByText(homepage.getGender(), "Male")
         homepage.getSubmit().click()
 
         alertText = homepage.getSuccessMessage().text
+        
+        assert ("Success" in alertText)
+        self.driver.refresh()
 
+    @pytest.fixture(params=[("firstN", "middleN", "lastN"),("firstN2", "middleN2", "lastN2")])
+    def getData(self, request):
+        return request.param
